03225232e0a12bf346849b62b4b6c5a1d01fb383
Amaan Rattansi <amaan@sameboat.ca>
2013-08-29 16:40:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sameboat::Application.config.secret_key_base = '1ac5e0f3e8c1b80d66751b792cc883b347a1fc755b944e7c972ab03d2d52bf3ddd4b2ccac272ffa06fad2f2f2b8ea74fdcea600c44770e3edf53953f08efb5b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sameboat::Application.config.session_store :cookie_store, key: '_sameboat_session'
