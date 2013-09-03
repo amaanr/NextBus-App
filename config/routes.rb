1aa7b2b5f427f885b992117b8148edfde06cb6db
Amaan Rattansi <amaan@sameboat.ca>
2013-09-03 11:29:16 -0400

    updated routes root to nearby index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Sameboat::Application.routes.draw do
+  get "nearby/index"
+  get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'nearby#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
