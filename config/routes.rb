10f250ade3c1b78d92bd51eb3671e2515a286b06
Amaan Rattansi <amaan@sameboat.ca>
2013-09-03 20:22:45 -0400

    removed nearby controller index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Sameboat::Application.routes.draw do
-  get "nearby/index"
   get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'nearby#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
