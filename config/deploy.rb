a6e7c4bf8b2d0ce52dfbea47dcafc111c5182c9c
Amaan Rattansi <amaan@sameboat.ca>
2013-10-31 16:20:03 -0400

    cap should be working

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,45 @@
-require "bundler/capistrano"
+set :application, 'Sameboat'
+set :user, 'sameboat'
+set :repo_url, 'git@github.com:amaanr/Sameboat.git'
 
-
-set :application, "Sameboat"
-set :user, "sameboat"
-
-
-set :scm, :git
-set :repository, "git@github.com:amaanr/Sameboat.git"
-set :branch, "master"
-set :use_sudo, true
-
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 server "sameboat.cloudapp.net", :web, :app, :db, primary: true
 
+set :deploy_to, '/home/#{user}/apps/#{application}'
+set :scm, :git
 
-set :deploy_to, "/home/#{user}/apps/#{application}"
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
-ssh_options[:port] = 22
+# set :format, :pretty
+# set :log_level, :debug
+# set :pty, true
 
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :keep_releases, 5
 
 namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
   desc "Fix permissions"
   task :fix_permissions, :roles => [ :app, :db, :web ] do
     run "chmod +x #{release_path}/config/unicorn_init.sh"
@@ -48,4 +67,5 @@ namespace :deploy do
   end
   after "deploy:finalize_update", "deploy:fix_permissions"
   after "deploy:finalize_update", "deploy:symlink_config"
+
 end
