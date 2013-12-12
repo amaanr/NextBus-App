53fc898d9eddc2945a601da3e596f048ffbf34e5
Amaan Rattansi <amaan@sameboat.ca>
2013-12-12 14:01:21 -0500

    Pulls

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,25 @@
 class PostsController < ApplicationController
 
 	def new
+		@posts = Post.all
 		@post = Post.new
+	end
+
+	def create
+		current_user.posts.create(post_params)
+		redirect_to new_post_path
+	end
+
+	def show
+		
+		@details = Post.find(params[:id])
+		redirect_to new_post_path
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:content)
 	end
 
 end
