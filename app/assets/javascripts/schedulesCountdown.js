c87a7e0f31f99289bc823e41e1311540ce78ae74
Amaan Rattansi <amaan.rattansi@gmail.com>
2014-05-14 18:05:18 -0400

    added setInterval for timer, need to add if logic when seconds hit 0

--- app/assets/javascripts/schedulesCountdown.js
+++ app/assets/javascripts/schedulesCountdown.js
@@ -1,18 +1,25 @@
-function makeMinSec (seconds) {
-	var minutes = Math.round((seconds - 30)/60);
-	var remainingSeconds = seconds % 60;
-	if (remainingSeconds < 10) {
-		remainingSeconds = "0" + remainingSeconds;
-	}
-
-	return minutes + ":" + remainingSeconds;
+function decorateSeconds (seconds) {
+  var minutes = Math.round((seconds - 30)/60);
+  var remainingSeconds = seconds % 60;
+  if (remainingSeconds < 10) {
+    remainingSeconds = "0" + remainingSeconds;
+  }
+	if (seconds == 0) {
+    clearInterval(countdownTimer);
+    $('.cellDepartsIn').html("Departing...");
+  } else {
+    seconds--;
+  }
+  return minutes + ":" + remainingSeconds;
 }
 
-function countDepartsIn() {
-	$('.cellDepartsIn').each(function () {
-		var count = $(this).data('seconds');
-		count --;
-		$(this).data('seconds', count);
-		$(this).text(makeMinSec(count));
-	});
+function tickDepartsIn() {
+  $('.cellDepartsIn').each(function () {
+    var count = $(this).data('seconds');
+    count --;
+    $(this).data('seconds', count);
+    $(this).text(decorateSeconds(count));
+  });
 }
+
+setInterval(function(){tickDepartsIn()},1000);
