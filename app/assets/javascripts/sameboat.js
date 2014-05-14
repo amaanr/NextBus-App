3c4d23650ae4863a728ae5a41b571755b48d9a08
Amaan Rattansi <amaan.rattansi@gmail.com>
2014-05-14 18:04:50 -0400

    removed countdown logic, separated jquery that populates table

--- app/assets/javascripts/sameboat.js
+++ app/assets/javascripts/sameboat.js
@@ -39,6 +39,9 @@ function showTransit(position) {
           success:
             function nextbusData(response) {
               var obj = $.xml2json(response);
+              // console.log(obj);
+              // var obj = response;
+              // console.log(obj);
 
               var timeInSeconds;
               var routeTag;
@@ -65,13 +68,38 @@ function showTransit(position) {
                               directionTitle = obj1.direction.title;
                               branchCode = obj2.branch;
                               directionCode = directionTitle.substring(0,1);
+
+                              var seconds = timeInSeconds;
+                              $(function countDown() {
+                                  var minutes = Math.round((seconds - 30)/60);
+                                  var remainingSeconds = seconds % 60;
+                                  if (remainingSeconds < 10) {
+                                      remainingSeconds = "0" + remainingSeconds;  
+                                  }
+                                  if (seconds == 0) {
+                                      clearInterval(countdownTimer);
+                                      $('#countdown').html("Departing...");
+                                  } else {
+                                      seconds--;
+                                  }
+                              });
                               
+                              function countDepartsIn() {
+                                $('.departs_in').each(function () {
+                                  var count = $(this).data('seconds');
+                                  count --;
+                                  $(this).data('seconds', count);
+                                  $(this).text(countDown(count));
+                                });
+                              }
+
                               $(function () {
                                 var n = directionTitle.indexOf("towards");
                                 var directionLength = directionTitle.length;
                                 heading = directionTitle.substring(n,directionLength);
-                                $("#schedules_table").find('tbody').append("<tr><td>" + directionCode + " " + branchCode + "</td><td>" + stopTitle + "</td><td>" + heading + "</td><td class='cellDepartsIn' data-seconds="+timeInSeconds+"></td></tr>");
+                                $("#schedules_table").find('tbody').append("<tr><td>" + directionCode + " " + branchCode + "</td><td>" + stopTitle + "</td><td>" + heading + "</td><td class='departs_in' data-seconds='" + timeInSeconds + "'>" + minutes + ":" + remainingSeconds + " s</td></tr>");
                               });
+  
                             };
                           } else {
                             for (var i = 0; i < obj1.direction.length; i++) {
@@ -92,7 +120,8 @@ function showTransit(position) {
                                       var n = directionTitle.indexOf("towards");
                                       var directionLength = directionTitle.length;
                                       heading = directionTitle.substring(n,directionLength);
-                                      $("#schedules_table").find('tbody').append("<tr><td>" + directionCode + " " + branchCode + "</td><td>" + stopTitle + "</td><td>" + heading + "</td><td class='cellDepartsIn' data-seconds="+timeInSeconds+"></td></tr>");
+                                      $("#schedules_table").find('tbody').append("<tr><td>" + directionCode + " " + branchCode + "</td><td>" + stopTitle + "</td><td>" + heading + "</td><td>" + timeInSeconds + " s</td></tr>");
+
                                     });
                                   }
                                 }  
@@ -119,14 +148,14 @@ function showTransit(position) {
                               branchCode = obj5.branch;
                               directionCode = directionTitle.substring(0,1);
 
-                              var seconds = timeInSeconds;
-
                               $(function () {
                                 var n = directionTitle.indexOf("towards");
                                 var directionLength = directionTitle.length;
                                 heading = directionTitle.substring(n,directionLength);
-                                $("#schedules_table").find('tbody').append("<tr><td>" + directionCode + " " + branchCode + "</td><td>" + stopTitle + "</td><td>" + heading + "</td><td class='cellDepartsIn' data-seconds="+timeInSeconds+"></td></tr>");
                               });
+
+                              $("#schedules_table").find('tbody').append("<tr><td>" + directionCode + " " + branchCode + "</td><td>" + stopTitle + "</td><td>" + heading + "</td><td class='cellDepartsIn' data-seconds="+timeInSeconds+"></td></tr>");
+                            
                             };
                           }; 
                         });
