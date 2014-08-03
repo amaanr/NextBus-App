cf50e84c94ebe6ab7c362221451184e24e1f9fcc
Amaan Rattansi <amaan.rattansi@gmail.com>
2014-08-03 15:39:34 -0400

    redirect logic for non-toronto users

--- app/assets/javascripts/sameboat.js
+++ app/assets/javascripts/sameboat.js
@@ -15,6 +15,9 @@ function showTransit(position) {
       "latitude": latitude,
       "longitude": longitude
     },
+    error: function(data) {
+      window.location.replace("http://sameboat.co/lakfd");
+    },
     success: function(response) {
       for (var i = 0; i < response.length; i++) {
 
