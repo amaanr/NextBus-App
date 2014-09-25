8f362749c70592505354e6fd38bc5e2dd0364019
Amaan Rattansi <amaan.rattansi@gmail.com>
2014-09-25 17:31:49 -0400

    added jquery to assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += ['css/style2.css', 'css/plugins2.css', 'js/plugins2.js', 'js/scripts2.js']
+Rails.application.config.assets.precompile += ['css/style2.css', 'css/plugins2.css', 'js/plugins2.js', 'js/scripts2.js','jquery.js']
