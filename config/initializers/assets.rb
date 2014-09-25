8ddc5057b999be89352d98c282b77c6d5ffa2fc6
Amaan Rattansi <amaan.rattansi@gmail.com>
2014-09-25 17:41:01 -0400

    added missing references to asset.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,3 @@
-Rails.application.config.assets.precompile += ['css/style2.css', 'css/plugins2.css', 'js/plugins2.js', 'js/scripts2.js','jquery.js']
+Rails.application.config.assets.precompile += ['css/style2.css', 'css/plugins2.css',
+	'js/plugins2.js', 'js/scripts2.js', 'jquery.js', 'slider/toronto5.jpg', 'pattern_dot.png', 
+'ico/icomoon0ef6.ttf']
