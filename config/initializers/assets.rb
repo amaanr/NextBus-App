7741e577903456711c7c3e42997deffec72bf2bb
Amaan Rattansi <amaan@sameboat.ca>
2014-09-27 04:53:41 +0000

    moved contents of assets.rb to production.rb

--- config/initializers/assets.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.assets.precompile += ['css/style2.css', 'css/plugins2.css',
-	'js/plugins2.js', 'js/scripts2.js', 'jquery.js', '/assets/slider/toronto5.jpg', '/assets/pattern_dot.png', 
-'/assets/ico/icomoon0ef6.ttf']
