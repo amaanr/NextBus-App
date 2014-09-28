aa482f9e8628c0e4376e7fbaaef0d373c8dcd1d1
Amaan Rattansi <amaan@sameboat.ca>
2014-09-28 04:30:00 +0000

    uncommented production.rb assets pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Sameboat::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( css/style2.css, css/plugins2.css, js/plugins2.js, js/scripts2.js, jquery.js )
+  config.assets.precompile += %w( 'css/style2.css', 'css/plugins2.css', 'js/plugins2.js', 'js/scripts2.js', 'jquery.js' )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
