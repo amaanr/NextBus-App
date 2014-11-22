fce7ec55cc59ef29988bd4a15e3dec65c14698d5
Amaan Rattansi <amaan@sameboat.ca>
2014-11-22 23:28:39 +0000

    setup vehicleLocations script, currently hard-coded directionCode and tag. need to pull selected values from select box nand pass in values to moveMarkers function

--- app/assets/javascripts/vehicleLocations.js
+++ app/assets/javascripts/vehicleLocations.js
@@ -36,57 +36,77 @@ function mapVehicles(routeList) {
 	    var scheduleXML;
 	    $.ajax({
 	        type: "GET",
-	        url: "http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=ttc&t=" + mvLastTime + "&r=" + route,
-	        dataType: "xml",
-	        success: xmlParser
+	        url: "http://webservices.nextbus.com/service/publicJSONFeed?command=vehicleLocations&a=ttc&t=" + mvLastTime + "&r=" + route,
+	        dataType: "JSON",
+	        success: function(data) {
+            var vehLocations = data["vehicle"];
+            var array = [];
+            var locations_object = {};
+            
+            for (var i = 0; i < vehLocations.length; i++) {
+              var dirTag = vehLocations[i].dirTag;
+              var heading = vehLocations[i].heading;
+              var id = vehLocations[i].id;
+              var lat = vehLocations[i].lat;
+              var lon = vehLocations[i].lon;
+              var routeTag = vehLocations[i].routeTag;
+              
+              locations_object = {
+                "dirTag": dirTag,
+                "heading": heading,
+                "id": id,
+                "lat": lat,
+                "lon": lon,
+                "routeTag": routeTag
+              }
+              
+              array.push(locations_object);
+            }
+            
+            // set markers
+            $.each(array, function(index, vehicle) {
+              var marker;
+              //if (vehicle.dirTag == mvDirectionTag) {
+	              text = 'Route (Bus Number): ' + vehicle.routeTag + ', vehicle id: ' + vehicle.id;
+	              marker = new google.maps.Marker({
+                  position: new google.maps.LatLng(vehicle.lat, vehicle.lon),
+                  title: text,
+                  id: vehicle.id
+			          });
+			
+			          // Set markers on the map
+                setMarkers(marker);
+              //}
+              
+            });
+            
+            function setMarkers(marker) {
+              var added = 0; // variable to check whether it's been dealt with or not
+
+              // See if the marker for same vehicle is in markerArray and replaces it
+              $.each(mvMarkerArray, function(i, storedMarker) {
+                if (marker.id == storedMarker.id) {
+                    storedMarker.setMap(null);
+                    marker.setMap(map);
+                  mvMarkerArray.splice(i, 1);
+                  mvMarkerArray.push(marker);
+                  added = 1;
+                }
+              });
+
+              // If it hasn't been replaced, marker is added to markerArray and set in map
+              if (added == 0){
+                  marker.setMap(map);
+                  mvMarkerArray.push(marker);	
+              }
+
+            }
+            
+          }
 	    })
 	});
-};
-
-function xmlParser(xml) {
-	var vehList = $.xml2json(xml);
-	// create a marker for each vehicle 
-	mvLastTime = vehList['lastTime']['time'];
-	vehList = vehList['vehicle'];
-	$.each(vehList, function(index, vehicle) {
-        var marker;
-        if (vehicle.dirTag == mvDirectionTag){
-	        text = 'Route (Bus Number): ' + vehicle.routeTag + ', vehicle id: ' + vehicle.id;
-	        marker = new google.maps.Marker({
-                position: new google.maps.LatLng(vehicle.lat, vehicle.lon),
-                title: text,
-                id: vehicle.id
-			});
-			
-			// Set markers on the map
-			setMarkers(marker);
-		}
-	});
-	
 }
 
-// function setMarkers(marker) {
-// 	var added = 0; // variable to check whether it's been dealt with or not
-	
-// 	// See if the marker for same vehicle is in markerArray and replaces it
-// 	$.each(mvMarkerArray, function(i, storedMarker) {
-// 		if (marker.id == storedMarker.id) {
-//     		storedMarker.setMap(null);
-//     		marker.setMap(map);
-// 			mvMarkerArray.splice(i, 1);
-// 			mvMarkerArray.push(marker);
-// 			added = 1;
-// 		}
-// 	});
-	
-// 	// If it hasn't been replaced, marker is added to markerArray and set in map
-// 	if (added == 0){
-//     	marker.setMap(map);
-//     	mvMarkerArray.push(marker);	
-// 	}
-	
-// }
-
-// moveVehicles(['95'],'95_1_95');
+moveVehicles(['122'],'122_0_122');
 
 google.maps.event.addDomListener(window, 'load', initialize);
